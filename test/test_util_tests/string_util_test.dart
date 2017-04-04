8e2a2c33b88e5d8ad7888658fa64b5fc8da9caae
Jace Hensley <jace.hensley@workiva.com>
2017-04-04 13:39:52 -0700

    Fix failing test

--- test/test_util_tests/string_util_test.dart
+++ test/test_util_tests/string_util_test.dart
@@ -3,7 +3,7 @@ import 'package:ui_test_utils/ui_test_utils.dart';
 
 /// Main entry point for string_util testing
 main() {
-  group('screamingSnakeToSpinal replaces all underscores with dashes and makes the string lowercase', () {
+  test('screamingSnakeToSpinal replaces all underscores with dashes and makes the string lowercase', () {
     expect(screamingSnakeToSpinal('TESTING_THE-screaming-snake_TO_spinal-method'),
         'testing-the-screaming-snake-to-spinal-method'
     );
