fdf599769c515e8ff96637bd96797f2077c05367
Jace Hensley <jace.hensley@workiva.com>
2017-04-05 16:40:09 -0700

    Simplify TestJacket rendering logic

--- lib/src/test_util/jacket.dart
+++ lib/src/test_util/jacket.dart
@@ -2,8 +2,6 @@ import 'dart:html';
 
 import 'package:over_react/over_react.dart' as over_react;
 import 'package:react/react.dart' as react;
-import 'package:react/react_client.dart' show ReactElement;
-import 'package:react/react_dom.dart' as react_dom;
 
 import 'package:ui_test_utils/src/test_util/react_util.dart' as react_util;
 
@@ -25,33 +23,35 @@ import 'package:ui_test_utils/src/test_util/react_util.dart' as react_util;
 /// Renders [node], and returns a `TestJacket` instance to use in a test.
 ///
 /// Will render into [mountNode] if provided.
-TestJacket<T> mount<T extends react.Component>(ReactElement node, {Element mountNode}) => new TestJacket<T>(node, mountNode: mountNode);
-
-/// Renders [node] attached to the document, and returns a `TestJacket` instance to use in a test.
-///
-/// Will render into [mountNode] if provided.
-TestJacket<T> mountToDocument<T extends react.Component>(ReactElement node, {Element mountNode}) => new TestJacket<T>.attached(node, mountNode: mountNode);
+TestJacket<T> mount<T extends react.Component>(dynamic node, {
+    Element mountNode,
+    bool attachedToDocument,
+    bool autoTearDown: true
+}) {
+  return new TestJacket<T>(node,
+      mountNode: mountNode,
+      attachedToDocument: attachedToDocument,
+      autoTearDown: autoTearDown
+  );
+}
 
 class TestJacket<T extends react.Component> {
   /* [1] */ Object _renderedInstance;
-  final Element _mountNode;
+  final Element mountNode;
+  final bool attachedToDocument;
+  final bool autoTearDown;
 
-  TestJacket(ReactElement node, {Element mountNode}) : this._mountNode = mountNode ?? new DivElement() {
+  TestJacket(dynamic node, {this.mountNode, this.attachedToDocument: false, this.autoTearDown: true}) {
     _render(node);
   }
 
-  TestJacket.attached(ReactElement node, {Element mountNode}) : this._mountNode = mountNode ?? new DivElement() {
-    if (document.contains(mountNode)) throw new StateError('');
-
-    document.body.append(_mountNode);
-    _render(node);
+  void _render(dynamic node) {
+    _renderedInstance = attachedToDocument
+        ? react_util.renderAttachedToDocument(node, container: mountNode, autoTearDown: autoTearDown)
+        : react_util.render(node, container: mountNode, autoTearDown: autoTearDown);
   }
 
-  void _render(ReactElement node) {
-    _renderedInstance = react_dom.render(node, _mountNode);
-  }
-
-  void rerender(ReactElement node) {
+  void rerender(dynamic node) {
     _render(node);
   }
 
@@ -69,10 +69,5 @@ class TestJacket<T extends react.Component> {
 
   void setState(newState, [callback]) {
     getDartInstance().setState(newState, callback);
-  }
-
-  void unmount() {
-    react_util.unmount(_mountNode);
-    _mountNode.remove();
   }
 }
