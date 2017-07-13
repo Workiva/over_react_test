59bb9c20b9e912f0a3a15d427fd7bec587a069f8
Aaron Lademann <aaron.lademann@workiva.com>
2017-07-13 10:59:55 -0700

    Add tests for commonComponentTests

--- test/over_react_test.dart
+++ test/over_react_test.dart
@@ -19,6 +19,7 @@ import 'package:over_react/over_react.dart';
 import 'package:react/react_client.dart';
 import 'package:test/test.dart';
 
+import 'over_react_test/common_component_util_test.dart' as common_component_util_test;
 import 'over_react_test/custom_matchers_test.dart' as custom_matchers_test;
 import 'over_react_test/dom_util_test.dart' as test_util_dom_util_test;
 import 'over_react_test/jacket_test.dart' as jacket_test;
@@ -30,6 +31,7 @@ main() {
 
   enableTestMode();
 
+  common_component_util_test.main();
   custom_matchers_test.main();
   test_util_dom_util_test.main();
   jacket_test.main();
---
+++ test/over_react_test/common_component_util_test.dart
@@ -0,0 +1,31 @@
+// Copyright 2017 Workiva Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:over_react/over_react.dart';
+import 'package:test/test.dart';
+import 'package:over_react_test/over_react_test.dart';
+
+import './utils/test_common_component.dart';
+
+/// Main entry point for [commonComponentTests] testing
+main() {
+  group('commonComponentTests', () {
+    // TODO: Improve / expand upon these tests.
+    group('should pass when the correct unconsumed props are specified', () {
+      commonComponentTests(TestCommon, unconsumedPropKeys: [
+        const $PropKeys(PropsThatShouldBeForwarded),
+      ]);
+    });
+  });
+}
