d7d74fe2130a9c0afeaec794b73c4c83962f41c3
Greg Littlefield <greg.littlefield@workiva.com>
2020-04-17 11:14:49 -0700

    Add regression test for factory() being called within group

--- test/over_react_test/common_component_util_test.dart
+++ test/over_react_test/common_component_util_test.dart
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import 'package:over_react/over_react.dart';
 import 'package:over_react_test/src/over_react_test/props_meta.dart';
 import 'package:test/test.dart';
 import 'package:over_react_test/over_react_test.dart';
@@ -78,6 +79,53 @@ main() {
           shouldTestClassNameOverrides: false,
           shouldTestPropForwarding: false,
       );
+      });
+    });
+
+    // This causes issues when consumers access properties on variables
+    // initialized in setUp blocks within `factory`.
+    group('does not call `factory` directly within the consuming group', () {
+      void sharedTest(
+        BuilderOnlyUiFactory factory, {
+        List Function(PropsMetaCollection) getUnconsumedPropKeys,
+      }) {
+        var wasFactoryCalledBeforeSetup = false;
+
+        // This needs to be before any setup within commonComponentTests is called,
+        // or it will fail when it shouldn't.
+        setUpAll(() {
+          expect(wasFactoryCalledBeforeSetup, isFalse,
+              reason: 'factory arg was called within group, '
+                  'before consumer setUp blocks are called');
+        });
+
+        commonComponentTests(() {
+          wasFactoryCalledBeforeSetup = true;
+          return factory();
+        }, getUnconsumedPropKeys: getUnconsumedPropKeys);
+      }
+
+      group('when passed a UiComponent', () {
+        // todo create a new component for this
+        sharedTest(() => TestCommonRequired()
+          ..bar = true
+          ..foobar = true);
+      });
+
+      group('when passed a UiComponent2', () {
+        group('(old boilerplate)', () {
+          // todo create a new component for this
+          sharedTest(() => TestCommonRequired2()
+            ..bar = true
+            ..foobar = true);
+        });
+
+        group('(new boilerplate)', () {
+          // todo create a new component for this
+          sharedTest(new_boilerplate.TestCommonForwarding, getUnconsumedPropKeys: (meta) => [
+            ...meta.forMixin(new_boilerplate.ShouldBeForwardedProps).keys,
+          ]);
+        });
       });
     });
   });
