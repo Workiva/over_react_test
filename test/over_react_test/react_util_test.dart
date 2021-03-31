5944d322d7dcf69c3bf9b3ab5611768bd5c14d1b
Keal Jones <41018730+kealjones-wk@users.noreply.github.com>
2021-03-31 11:36:50 -0700

    Apply suggestions from code review
    Co-authored-by: Aaron Lademann <aaron.lademann@workiva.com>

--- test/over_react_test/react_util_test.dart
+++ test/over_react_test/react_util_test.dart
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'dart:developer';
 import 'dart:html';
 
 import 'package:over_react/over_react.dart';
